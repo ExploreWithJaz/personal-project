ae5ab9513db85519320e362e17be5cc8eb82b314
ExploreWithJaz <51773574+ExploreWithJaz@users.noreply.github.com>
2024-01-17 09:41:45 +0000

    Modified the demo.dart

--- demo.dart
+++ demo.dart
@@ -1,1 +1,25 @@
-Hello
+import 'dart:io';
+
+void main() {
+  int ctr = 0;
+  int passcode = 1234;
+
+  do {
+    stdout.write('Enter your password : ');
+    int userPass = int.parse(stdin.readLineSync()!);
+
+    if (userPass != passcode) {
+      print('Access Denied!');
+      ctr++;
+    }
+    else{
+      print('Access Granted!!!');
+      break;
+    }
+  }
+  while(ctr!=3);
+  
+  if(ctr == 3) {
+    print('\nACCCOUNT LOCKED');
+  } 
+}
